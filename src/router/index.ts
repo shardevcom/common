9d2ed2f30ca3f537e7c9f50c929b1be12478a7c1
Elbert Tous <elbertjose@hotmail.com>
2025-07-30 08:20:39 -0500

    fix redirect

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ export * from './types';
 export * from './components';
 export * from './parser-routes';
 export * from './provider';
+export * from './context';
 export * from './router-dom-export';
 
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -3,3 +3,4 @@ export * from './provider';
 export * from './hooks';
 export * from './factory';
 export * from './context';
+export * from './slices/auth.slice';
