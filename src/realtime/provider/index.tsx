df3ca7e00a249dc2dffb717ffdb437d66e556939
Elbert Tous <elbertjose@hotmail.com>
2026-04-30 04:38:20 -0500

    fix reverb adapter

--- src/realtime/provider/index.tsx
+++ src/realtime/provider/index.tsx
@@ -1,20 +1,28 @@
+// En @shardev/common, modifica la definición del Provider
+// realTime.provider.tsx
 import React from 'react';
 import {RealtimeAdapter} from "../types";
 import {RealtimeContext} from "../context";
 
 interface DataProviderProps {
-    adapter: RealtimeAdapter;
+    adapter: RealtimeAdapter | null; // 👈 Permitir null
     children: React.ReactNode;
 }
 
 export const RealTimeProvider: React.FC<DataProviderProps> = ({ adapter, children }) => {
+    // Proveer un adapter dummy si es null para evitar crashes
+    const safeAdapter = adapter || {
+        subscribe: async () => ({ unsubscribe: () => {} }),
+        connect: () => {},
+        disconnect: () => {},
+        unsubscribe: () => {},
+    };
+
     return (
-        <RealtimeContext.Provider value={{ adapter }}>
+        <RealtimeContext.Provider value={{ adapter: safeAdapter }}>
             {children}
         </RealtimeContext.Provider>
     );
 };
-
-RealTimeProvider.displayName = "RealTimeProvider";
 
 
